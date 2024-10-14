a68a50f622c9e37b5df039f024166ba6583edb9f
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-10-14 09:13:15 -0400

    Implement the complete layout

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,7 +1,33 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "John", lastName: "Doe" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <h1>
+            <HeaderBox
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName + " " + loggedIn?.lastName || "Guest"}
+              subtext="Access and manage your account and transactions"
+            />
+          </h1>
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.5}
+          />
+        </header>
+        <RightSidebar />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">, {user}</span>
+        )}
+        <p className="header-box-subtext">{subtext}</p>
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderBox;
